11e309b7e2249bdf5847ece1f0e6b359d65344e7
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-05-27 15:12:03 +0600

    functional bank

---
+++ banking.js
@@ -0,0 +1,56 @@
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
+    const inputValue = parseFloat(inputField.value);
+
+    inputField.value = '';
+
+    return inputValue;
+}
+
+function getInnerTextValue(id) {
+    const fieldTag = document.getElementById(id);
+    const fieldTagText = fieldTag.innerText;
+    const value = parseFloat(fieldTagText);
+    return value;
+}
+
+function updateTotal(fieldId, amount) {
+    previousTotal = getInnerTextValue(fieldId);
+    const newTotal = previousTotal + amount;
+    document.getElementById(fieldId).innerText = newTotal;
+}
+
+
+// function updateTotal(fieldId, amount) {
+//     const fieldTag = document.getElementById(fieldId);
+//     const previousTotalInText = fieldTag.innerText;
+//     const previousTotal = parseFloat(previousTotalInText);
+//     const newTotal = previousTotal + amount;
+//     fieldTag.innerText = newTotal;
+// }
+
+function updateBalance() {
+
+}
+
+console.log(updateBalance());
+
+// add event handler on deposit button 
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const amount = getInputValue('deposit-input');
+    console.log(amount);
+    if (amount > 0) {
+        updateTotal('deposit-money', amount);
+
+    }
+})
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const amount = getInputValue('withdraw-input');
+
+    if (amount > 0) {
+        updateTotal('withdraw-money', amount);
+
+    }
+})
+
